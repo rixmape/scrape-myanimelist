3509d63559565e2b0e39ff724b46c292ab3125b4
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-05-27 20:29:49 +0800

    Update to scrape anime single page

--- myanimelist/spiders/topanime.py
+++ myanimelist/spiders/topanime.py
@@ -8,7 +8,31 @@ class TopanimeSpider(scrapy.Spider):
 
     def parse(self, response):
         for anime in response.css(".ranking-list"):
-            yield {
-                "title": anime.css("h3 a::text").get(),
-                "url": anime.css("h3 a::attr(href)").get(),
-            }
+            url = anime.css("h3 a::attr(href)").get()
+            yield scrapy.Request(url, callback=self.parse_anime)
+
+    def parse_anime(self, response):
+        title = response.css(".title-name strong::text").get()
+        title_english = response.css(".title-english::text").get()
+        score = response.css(".score-label::text").get()
+        rank = response.css(".ranked::text").get()
+        popularity = response.css(".popularity::text").get()
+        description_texts = response.css(
+            "p[itemprop='description']::text"
+        ).getall()
+        members = response.css(".members strong::text").get()
+        yield {
+            "title": title,
+            "title_english": title_english,
+            "score": score,
+            "rank": rank,
+            "popularity": popularity,
+            "description": " ".join(
+                [
+                    clean_text
+                    for text in description_texts
+                    if (clean_text := text.strip())
+                ]
+            ),
+            "members": members,
+        }
