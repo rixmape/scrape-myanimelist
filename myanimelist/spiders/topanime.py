0c56538a10d398d54dd8a203cd2ffee5fb1e6c4f
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-05-29 08:04:12 +0800

    Add pagination handling to scrape k animes

--- myanimelist/spiders/topanime.py
+++ myanimelist/spiders/topanime.py
@@ -7,11 +7,23 @@ class TopanimeSpider(scrapy.Spider):
     name = "topanime"
     allowed_domains = ["myanimelist.net"]
     start_urls = ["https://myanimelist.net/topanime.php"]
+    anime_limit = 500
+    anime_count = 0
 
     def parse(self, response):
         for anime in response.css(".ranking-list"):
             url = anime.css("h3 a::attr(href)").get()
             yield scrapy.Request(url, callback=self.parse_anime)
+            self.anime_count += 1
+
+            if self.anime_count >= self.anime_limit:
+                break
+
+        if self.anime_count < self.anime_limit:
+            next_page_url = response.css("a.next::attr(href)").get()
+            if next_page_url is not None:
+                next_page_url = response.urljoin(next_page_url)
+                yield scrapy.Request(next_page_url, callback=self.parse)
 
     def parse_anime(self, response):
         item = AnimeItem()
